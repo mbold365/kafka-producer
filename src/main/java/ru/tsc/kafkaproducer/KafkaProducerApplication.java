44083bd7b378c12926d4d0b0d9f01fe6f141e1fb
Maxim Boldaev <mboldaev@tsconsulting.com>
2021-01-20 13:41:49 +0300

    Enable async in KafkaProducerApplication

--- src/main/java/ru/tsc/kafkaproducer/KafkaProducerApplication.java
+++ src/main/java/ru/tsc/kafkaproducer/KafkaProducerApplication.java
@@ -2,12 +2,13 @@ package ru.tsc.kafkaproducer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class KafkaProducerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(KafkaProducerApplication.class, args);
     }
-
 }
