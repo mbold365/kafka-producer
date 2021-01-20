586d1d4dad430555255e9cbecc8b0f800ba75002
Maxim Boldaev <mboldaev@tsconsulting.com>
2021-01-20 13:40:26 +0300

    Add hazelcast config

--- src/main/java/ru/tsc/kafkaproducer/config/HazelcastConfig.java
+++ src/main/java/ru/tsc/kafkaproducer/config/HazelcastConfig.java
@@ -1,8 +1,13 @@
 package ru.tsc.kafkaproducer.config;
 
-import com.hazelcast.client.HazelcastClient;
-import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.config.Config;
+import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.map.IMap;
+import com.hazelcast.spring.cache.HazelcastCacheManager;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,13 +16,31 @@ import org.springframework.context.annotation.Configuration;
 @EnableCaching
 public class HazelcastConfig {
 
+    private static final String HC_INSTANCE_BEAN_NAME = "hcInstance";
+
     @Bean
-    public HazelcastInstance hazelcastInstance(ClientConfig clientConfig) {
-        return HazelcastClient.newHazelcastClient(clientConfig);
+    CacheManager cacheManage(@Qualifier(HC_INSTANCE_BEAN_NAME) HazelcastInstance hcInstance) {
+        return new HazelcastCacheManager(hcInstance);
+
+    }
+
+    @Bean(HC_INSTANCE_BEAN_NAME)
+    public HazelcastInstance hazelcastInstance(Config config) {
+        return Hazelcast.newHazelcastInstance(config);
     }
 
     @Bean
-    public ClientConfig clientConfig() {
-        return new ClientConfig();
+    public Config config() {
+        Config config = new Config();
+        config.getNetworkConfig().getJoin().getTcpIpConfig().setEnabled(true).addMember("127.0.0.1:5701");
+        config.getNetworkConfig().getJoin().getMulticastConfig().setEnabled(false);
+        return config;
+    }
+
+    @Bean
+    public IMap<Long, byte[]> configMap(
+            @Qualifier(HC_INSTANCE_BEAN_NAME) HazelcastInstance hcInstance,
+            @Value("${hazelcast.map.name}") String configMapName) {
+        return hcInstance.getMap(configMapName);
     }
 }
